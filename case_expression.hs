8c34d9061a7994845b56cabac959d784672f90af
Kwe <Kweayon@gmail.com>
2024-03-20 21:07:16 -0500

    feat: edited comments

--- case_expression.hs
+++ case_expression.hs
@@ -1,12 +1,11 @@
--- TODO: edit comments
-
 -- Case Expressions | Pattern Matching Anywhere
 
+--   - pattern matches a variable and evaluates pieces of code based on its value
 --   - similar to CASE statements in SQL or Switch statments in imperative programming languages
---   - case expressions are as there nsame implies expressions
---   - pattern matches a variable and evaluates peices of code based on its value
---   - since Case Expressions are expression they can be used almost anywhere to match an expression to a parameter
---   - just like Pattern Matching on Function Parameters Case Expression require an exhaustive list of values of runtime errors will be thrown on values that do not match a pattern
+--   - case expressions are expressions
+--   - can be used anywhere an expression can be used
+--   - similar to Pattern Matching on Function Parameters Case Expressions
+--     require an exhaustive list of values or a runtime error will be thrown
 --   - useful for pattern matching something in the middle of an expression
 
 -- Case Expression Syntax
@@ -30,6 +29,8 @@ describeList xs =
     xs -> "a list with " ++ show (length xs) ++ " elements."
 
 -- Equivalent Pattern Matching on a Function Parameter Using where binding
+
+--   - where bindings can also pattern match in the middle of an expression contained in a function
 
 describeList' :: [a] -> String
 describeList' xs = "the list is " ++ describe xs
